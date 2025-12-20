fe1aeffa9595b4fe92007d925fd87eeef2d9be45
Priyanka Surve <priyankasurve1925@gmail.com>
2025-12-20 19:45:41 +0530

    Add files via upload

---
+++ Basic_Program/height and weight.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  float Side = 0.0, Area=0.0;
+  printf("\n Enter Side of Square");
+  scanf("%f", &Side);
+  
+  Area= Side * Side;
+  printf("\n Area of Square is:%f",Area);
+  getch();
+  return 0;
+} printf("\n Area of rectangle is:%f",Area);
+  getch();
+  return 0;
+}
