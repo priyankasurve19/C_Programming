4f4c4364c8ad7e2c5abda084a11e9b8df3d43a14
Priyanka Surve <priyankasurve1925@gmail.com>
2025-12-20 19:44:22 +0530

    Add files via upload

---
+++ Basic_Program/Program of Addition.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num1=0, Num2=0, Sum=0;
+  printf("\n Enter 1st number= ");
+  scanf("%d",&Num1);
+  printf("\n Enter 2nd number");
+  scanf("%d",&Num2);
+  
+   Sum= Num1+Num2;
+   
+   printf ("\n Addition = > %d+%d=%d", Num1,Num2,Sum);
+   getch();
+   return 0;
+ }
+
+   
+  
+  
+  
+  
+  
+}
