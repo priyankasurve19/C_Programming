7117a30929a60e1f6dd022a8156e325e990b85e7
Priyanka Surve <priyankasurve1925@gmail.com>
2025-12-22 14:47:10 +0530

    Add files via upload

---
+++ Basic_Program/Program of cube.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+   int Num=0, Cube=0;
+   
+   printf("\n Enter the number");
+   scanf("%d",&Num);
+   
+   Cube= Num * Num * Num;
+   
+   printf("\n Cube of number = %d",Cube);
+   getch();
+   return 0;
+ }
+   
+   
+  
+  
