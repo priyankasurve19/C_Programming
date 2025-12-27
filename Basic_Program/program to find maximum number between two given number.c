efcb32fa7411236926af13b45ed7ad2eeba5d393
Priyanka Surve <priyankasurve1925@gmail.com>
2025-12-27 11:05:26 +0530

    Add files via upload

---
+++ Basic_Program/program to find maximum number between two given number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+  int a=45,b=23;
+  printf("Enter two numbers");
+  scanf("%d,%d",&a,&b);
+  
+   if(a>b)
+   printf("The greater number is: %d",a);
+   printf("The greater number is:%d",a);
+   getch();
+   return 0;
+}
+   
+  
+  
+  
+   
+  
+   
+ 
+   
