64679776de477f056bfe903bb129a7ce7b8122ef
Priyanka Surve <priyankasurve1925@gmail.com>
2025-12-27 13:19:38 +0530

    Add files via upload

---
+++ Basic_Program/program of even or odd number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int Num;
+   clrscr();
+   printf("Enter the number: ");
+   scanf("%d",Num);
+   
+   if(Num%0==0)
+   {
+     printf("Number is even");
+    }
+    else if(Num%0==0)
+    {
+      printf("Number is even");
+      
+    }
+    else
+    {
+      printf("Number is zero:");
+     }
+     getch();
+     return 0;
+ }
+    
+   
+   
+   
