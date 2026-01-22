d03d20fd1ac481110ecf3a434dfe4f187d802c70
Priyanka Surve <priyankasurve1925@gmail.com>
2026-01-22 11:10:06 +0530

    Add files via upload

---
+++ Assignments/Assignment No.01/Program to print multiplication of two number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int Num1=0, Num2=0, Mul=0;
+   
+   printf("\n Enter 1st Number ");
+   scanf("%d",&Num1);
+   
+   printf("\n Enter 2nd Number ");
+   scanf("%d",&Num2);
+   
+   Mul=Num1*Num2;
+   
+   printf("\n Multiplication:%d*%d=%d",Num1,Num2,Mul);
+   getch();
+   return 0;
+ }
+  
+  
+  
