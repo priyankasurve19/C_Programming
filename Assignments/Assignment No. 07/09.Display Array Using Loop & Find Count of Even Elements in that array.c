8b9fd3e2c23d71a2ec978fb4e4bd16c6b27b392d
Priyanka Surve <priyankasurve1925@gmail.com>
2026-02-01 15:13:02 +0530

    Add files via upload

---
+++ Assignments/Assignment No. 07/09.Display Array Using Loop & Find Count of Even Elements in that array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 5
+int main()
+{
+   int Bill[Size] = {},i = 0,E_Cnt = 0;
+
+   for(i=0; i < Size; i++)
+   {
+    printf("\n Enter Bill No %d:",i+1);
+    scanf("%d",&Bill[i]);
+
+    if(Bill[i]%2==0)
+    {
+       E_Cnt++;
+    }
+ }
+    printf("\n Even Count of all bills = %d.",E_Cnt);
+
+    getch();
+    return 0;
+ }
